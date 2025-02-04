95eb75ab28954872cd8a6f2f9b0bbbb8d573e1ee
tesarlukas <lukastesar90@gmail.com>
2025-02-04 23:17:37 +0100

    feat: add Resizable

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,38 @@
 import { Editor } from "@/components/Editor";
-import { Provider } from "./provider";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/Resizable";
 import { AppearanceSettings } from "@/features/appearance/views/AppearanceSettings";
-import { Button } from "@/components/ui/Button";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { Provider } from "./provider";
+import { useColorTheme } from "@/features/appearance/colorTheme/hooks/useColorTheme";
 
 export default function App() {
+  const { t } = useTranslation();
+  const { updateColorTheme } = useColorTheme();
+
+  useEffect(() => {
+    const setupTheme = async () => {
+      await updateColorTheme();
+    };
+
+    setupTheme();
+  }, []);
+
   return (
     <main className="h-screen justify-center bg-fuchsia-100 p-4">
       <Provider>
         <Editor />
         <AppearanceSettings />
+        <div>{t("helloWorld")}</div>
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel>One</ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel>Two</ResizablePanel>
+        </ResizablePanelGroup>
       </Provider>
     </main>
   );

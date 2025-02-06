81e946cf4bd7ff6617c0a767abb102456f1fdb93
tesarlukas <lukastesar90@gmail.com>
2025-02-06 18:27:07 +0100

    feat: bootstrap button, card and resizable through shadcn cli

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+/** This is a generated file from shadcn/ui **/
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }

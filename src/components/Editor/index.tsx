101af6d85806f21db9f2f8dc7bd290fdabd0f458
tesarlukas <lukastesar90@gmail.com>
2025-02-09 17:18:16 +0100

    feat: introduce bullet list styling

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -9,11 +9,23 @@ import StarterKit from "@tiptap/starter-kit";
 import { Image } from "@tiptap/extension-image";
 import { Button } from "../ui/button";
 import Code from "@tiptap/extension-code";
+import ListItem from "@tiptap/extension-list-item";
+import BulletList from "@tiptap/extension-bullet-list";
 
 // define your extension array
-const extensions = [StarterKit, Image, Code];
+const extensions = [
+  StarterKit,
+  Image,
+  Code,
+  ListItem,
+  BulletList.configure({
+    HTMLAttributes: {
+      class: "list-disc ml-2",
+    },
+  }),
+];
 
-const content = "<p>Hello World!</p>";
+const content = " <ul> <li>A list item</li> <li>And another one</li> </ul>";
 
 export const Editor = () => {
   const editor = useEditor({

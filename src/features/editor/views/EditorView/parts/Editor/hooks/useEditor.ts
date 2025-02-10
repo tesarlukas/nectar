0e4c4401c49161ced1925772c23290b6d6c25890
tesarlukas <lukastesar90@gmail.com>
2025-02-10 20:55:40 +0100

    feat: add image drag and drop feature

--- src/features/editor/views/EditorView/parts/Editor/hooks/useEditor.ts
+++ src/features/editor/views/EditorView/parts/Editor/hooks/useEditor.ts
@@ -19,6 +19,7 @@ import Dropcursor from "@tiptap/extension-dropcursor";
 import Gapcursor from "@tiptap/extension-gapcursor";
 import CharacterCount from "@tiptap/extension-character-count";
 import History from "@tiptap/extension-history";
+import FileHandler from "@tiptap-pro/extension-file-handler";
 
 const extensions = [
   // Nodes
@@ -123,6 +124,54 @@ const extensions = [
   }),
   CharacterCount,
   History,
+  FileHandler.configure({
+    allowedMimeTypes: ["image/png", "image/jpeg", "image/gif", "image/webp"],
+    onDrop: (currentEditor, files, pos) => {
+      files.forEach((file) => {
+        const fileReader = new FileReader();
+
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+          currentEditor
+            .chain()
+            .insertContentAt(pos, {
+              type: "image",
+              attrs: {
+                src: fileReader.result,
+              },
+            })
+            .focus()
+            .run();
+        };
+      });
+    },
+    onPaste: (currentEditor, files, htmlContent) => {
+      files.forEach((file) => {
+        if (htmlContent) {
+          // if there is htmlContent, stop manual insertion & let other extensions handle insertion via inputRule
+          // you could extract the pasted file from this url string and upload it to a server for example
+          console.log(htmlContent); // eslint-disable-line no-console
+          return false;
+        }
+
+        const fileReader = new FileReader();
+
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+          currentEditor
+            .chain()
+            .insertContentAt(currentEditor.state.selection.anchor, {
+              type: "image",
+              attrs: {
+                src: fileReader.result,
+              },
+            })
+            .focus()
+            .run();
+        };
+      });
+    },
+  }),
 ];
 
 const content = `

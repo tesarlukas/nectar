2df4535e7321b2e9efec384218a423e5a32087f5
tesarlukas <lukastesar90@gmail.com>
2025-02-15 14:16:24 +0100

    feat(TopMenu): add hivename to the topmenu

--- src/components/TopMenu/index.tsx
+++ src/components/TopMenu/index.tsx
@@ -3,10 +3,12 @@ import { Settings, Pencil } from "lucide-react";
 import { Card } from "../ui/card";
 import { ColorSchemeToggle } from "./parts/ColorSchemeToggle";
 import { Button } from "../ui/button";
+import { useHiveStore } from "@/stores/useHiveStore";
 
 export const TopMenu = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
+  const hiveName = useHiveStore((state) => state.hiveName)
   const isInSettings = pathname.includes("settings");
 
   const handleOnSettingsToggle = () => {
@@ -22,7 +24,7 @@ export const TopMenu = () => {
     <>
       <Card className="rounded-b-none py-4 px-8 flex flex-row">
         <div>
-          <h1 className="text-4xl">Nectar</h1>
+          <h1 className="text-4xl">{hiveName}</h1>
         </div>
         <div className="flex flex-row ml-auto gap-x-2">
           <Button

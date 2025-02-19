808eaa44e05e49946ac744e5b68885273f07a60d
tesarlukas <lukastesar90@gmail.com>
2025-02-19 19:22:25 +0100

    feat: strip json from the notetitle

--- src/features/editor/views/EditorView/parts/NoteTitle/index.tsx
+++ src/features/editor/views/EditorView/parts/NoteTitle/index.tsx
@@ -1,4 +1,4 @@
-//import { EditableText } from "@/components/EditableText";
+import { stripJson } from "@/utils/nodeHelpers";
 
 interface NoteTitleProps {
   title?: string;
@@ -8,7 +8,7 @@ export const NoteTitle = ({ title }: NoteTitleProps) => {
   return (
     <>
       <div className="flex flex-pow justify-center items-center text-2xl mt-4">
-        {title}
+        {stripJson(title)}
       </div>
     </>
   );
--- src/utils/nodeHelpers.ts
+++ src/utils/nodeHelpers.ts
@@ -2,6 +2,6 @@ export const appendJson = (value: string) => {
   return value.endsWith(".json") ? value : `${value}.json`;
 };
 
-export const stripJson = (value: string) => {
-  return value.substring(0, value.lastIndexOf("."));
+export const stripJson = (value?: string) => {
+  return value?.substring(0, value.lastIndexOf("."));
 };

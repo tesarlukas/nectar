77d17faeaf25abda4390462063aa0087084c51af
tesarlukas <lukastesar90@gmail.com>
2025-03-06 14:09:56 +0100

    feat(ShortcutsSettings): lift the shortcuts and the state up

--- src/features/settings/views/ShortcutsSettings/index.tsx
+++ src/features/settings/views/ShortcutsSettings/index.tsx
@@ -1,19 +1,55 @@
 import { Button } from "@/components/ui/button";
 import { useShortcutsStore } from "@/stores/useShortcutStore/index";
-import { useCallback } from "react";
+import { useCallback, useMemo, useRef } from "react";
 import { ShortcutItem } from "./parts/ShortcutItem";
 import { useTranslation } from "react-i18next";
 import { Typography } from "@/components/Typography";
+import { useRecordHotkeys } from "react-hotkeys-hook";
+import { useShortcuts } from "@/features/shortcuts/hooks/useShortcuts";
+import type { ActionId } from "@/features/events/eventEmitter";
 
 export const ShortcutsSettings = () => {
   const { t } = useTranslation(["settings", "common"]);
   const shortcuts = useShortcutsStore((state) => state.shortcuts);
 
   const reset = useShortcutsStore((state) => state.resetToDefault);
-
   const handleOnReset = useCallback(() => {
     reset();
   }, []);
+
+  const [
+    recordedKeys,
+    { start: startRecording, stop: stopRecording, isRecording },
+  ] = useRecordHotkeys();
+  const recordedActionRef = useRef<ActionId>(null);
+  const newKeysRef = useRef("");
+
+  const handleShortcutItemOnClick = (actionId: ActionId) => {
+    recordedActionRef.current = actionId;
+    startRecording();
+  };
+
+  const getRecordedKeysOfAction = useCallback(
+    (actionId: ActionId) => {
+      if (actionId === recordedActionRef.current) {
+        return recordedKeys;
+      }
+    },
+    [recordedKeys, recordedActionRef],
+  );
+
+  const formattedKeys = useMemo(() => {
+    const chars = [...recordedKeys];
+    const joinedChars = chars.join("+");
+
+    return joinedChars;
+  }, [recordedKeys]);
+
+  useShortcuts("escape", () => stopRecording());
+  useShortcuts("enter", () => {
+    newKeysRef.current = formattedKeys;
+    stopRecording();
+  });
 
   return (
     <>
@@ -31,6 +67,11 @@ export const ShortcutsSettings = () => {
               t={t}
               shortcut={shortcut}
               actionId={actionId}
+              onClick={handleShortcutItemOnClick}
+              isRecording={
+                isRecording && actionId === recordedActionRef.current
+              }
+              recordedKeys={getRecordedKeysOfAction(actionId)}
             />
           ))}
         </div>
--- src/features/settings/views/ShortcutsSettings/parts/ShortcutItem/index.tsx
+++ src/features/settings/views/ShortcutsSettings/parts/ShortcutItem/index.tsx
@@ -3,61 +3,51 @@ import { Card, CardTitle } from "@/components/ui/card";
 import type { ActionId } from "@/features/events/eventEmitter";
 import type { KeyboardShortcut } from "@/stores/useShortcutStore/index.preset";
 import { splitShortcut } from "@/stores/useShortcutStore/utils/shortcutHelpers";
-import { useRecordHotkeys } from "react-hotkeys-hook";
-import { useShortcuts } from "@/features/shortcuts/hooks/useShortcuts";
-import { useMemo, useRef } from "react";
+import { useMemo } from "react";
 
 interface ShortcutItemProps {
   t: TFunction;
   shortcut: KeyboardShortcut;
   actionId: ActionId;
+  onClick: (actionId: ActionId) => void;
+  isRecording: boolean;
+  recordedKeys?: Set<string>;
 }
 
-export const ShortcutItem = ({ t, shortcut, actionId }: ShortcutItemProps) => {
-  const [
-    recordedKeys,
-    { start: startRecording, stop: stopRecording, isRecording },
-  ] = useRecordHotkeys();
-  const newKeysRef = useRef("");
+export const ShortcutItem = ({
+  t,
+  shortcut,
+  actionId,
+  onClick,
+  isRecording,
+  recordedKeys,
+}: ShortcutItemProps) => {
+  const shortcutPart = splitShortcut(shortcut)[1];
 
-  const handleOnCardClick = () => {
-    startRecording();
-  };
-  useShortcuts("escape", () => stopRecording());
   const formattedKeys = useMemo(() => {
+    if (!recordedKeys) return shortcutPart;
+
     const chars = [...recordedKeys];
     const joinedChars = chars.join("+");
 
     return joinedChars;
   }, [recordedKeys]);
 
-  // TODO: these shortcuts gonna be lifted
-  const enterRef = useShortcuts(
-    "enter",
-    () => {
-      newKeysRef.current = formattedKeys;
-      stopRecording();
-      console.log(newKeysRef.current);
-    },
-    { scopes: "scopeA" },
-  );
-
   return (
     <>
       <Card
-        ref={enterRef}
         tabIndex={-1}
         className="p-4 flex flex-row justify-between hover:bg-accent
         hover:text-accent-foreground cursor-pointer active:bg-accent/90
         transition-all duration-200 active:scale-[0.98] border border-border
         focus:outline-none"
-        onClick={handleOnCardClick}
+        onClick={() => onClick(actionId)}
       >
         <CardTitle>{t(`shortcuts.${actionId}`)}</CardTitle>
         <CardTitle
           className={`capitalize ${isRecording ? "text-primary animate-pulse" : ""}`}
         >
-          {isRecording ? formattedKeys : splitShortcut(shortcut)[1]}
+          {isRecording ? formattedKeys : shortcutPart}
         </CardTitle>
       </Card>
     </>

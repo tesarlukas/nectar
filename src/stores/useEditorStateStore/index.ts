1a3c86038823713dc1e107e79ff4a419e87403eb
tesarlukas <lukastesar90@gmail.com>
2025-03-08 18:38:45 +0100

    feat: create editorStateStore

---
+++ src/stores/useEditorStateStore/index.ts
@@ -0,0 +1,23 @@
+import type { EditorState } from "@tiptap/pm/state";
+import { create } from "zustand";
+
+export type EditorStates = Record<string, EditorState>;
+
+export interface EditorStateStore {
+  editorStates: EditorStates;
+}
+
+export const useEditorStateStore = create<EditorStateStore>((set) => ({
+  editorStates: {},
+  addEditorState: (id: string, editorState: EditorState) =>
+    set((state) => ({
+      editorStates: { ...state.editorStates, [id]: editorState },
+    })),
+  removeEditorState: (id: string) =>
+    set((state) => {
+      const newEditorStates = { ...state.editorStates };
+      delete newEditorStates[id];
+      return { editorStates: newEditorStates };
+    }),
+  clearEditorStates: () => set(() => ({ editorStates: {} })),
+}));

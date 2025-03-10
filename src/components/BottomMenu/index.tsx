5d900ca65f6f0339586d822e946047bcc6aa17a8
tesarlukas <lukastesar90@gmail.com>
2025-03-10 19:57:51 +0100

    feat(BottomMenu): translations

--- src/components/BottomMenu/index.tsx
+++ src/components/BottomMenu/index.tsx
@@ -10,7 +10,7 @@ export const BottomMenu = ({
   charCount,
   children,
 }: PropsWithChildren<BottomMenuProps>) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation("editorView");
 
   return (
     <>

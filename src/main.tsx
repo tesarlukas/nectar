e88c1b80d2d49a23db4de4b304a68431b2d1e476
tesarlukas <lukastesar90@gmail.com>
2025-04-08 12:42:38 +0200

    feat: disable context menu and ctrl+j in production

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,29 @@ import "./main.css";
 
 import "./translation";
 
+// // Prevent context menu only in production
+if (import.meta.env.PROD) {
+  document.addEventListener(
+    "contextmenu",
+    (e) => {
+      e.preventDefault();
+      return false;
+    },
+    false,
+  );
+
+  // Prevent Ctrl+J shortcut
+  document.addEventListener(
+    "keydown",
+    (e) => {
+      if (e.ctrlKey && e.key === "j") {
+        e.preventDefault();
+      }
+    },
+    false,
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
